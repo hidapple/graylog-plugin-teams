d1f69536bf817b53e1c06d19149f00a384ce2e11
hidapple <shotfsh@gmail.com>
2019-03-21 20:23:15 +0900

    Remove web related files

---
+++ src/test/java/org/graylog/plugins/teams/client/TeamsMessageCardTest.java
@@ -0,0 +1,16 @@
+package org.graylog.plugins.teams.client;
+
+import org.junit.jupiter.api.Test;
+
+class TeamsMessageCardTest {
+
+  @Test
+  void toJsonString_ConvertToJsonString() {
+//    TeamsMessageCard sut = new TeamsMessageCard("test message");
+//    String expected = "{\"text\":\"test message\"}";
+//
+//    String actual = sut.toJsonString();
+//
+//    Assertions.assertEquals(expected, actual);
+  }
+}
