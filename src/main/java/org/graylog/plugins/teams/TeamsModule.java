21e72cbb44aeb8c029f3eaae9f7f5cf84560472c
hidapple <shotfsh@gmail.com>
2019-09-08 22:24:30 +0900

    Support event notification for Graylog3.1

--- src/main/java/org/graylog/plugins/teams/TeamsModule.java
+++ src/main/java/org/graylog/plugins/teams/TeamsModule.java
@@ -1,5 +1,7 @@
 package org.graylog.plugins.teams;
 
+import org.graylog.events.notifications.EventNotification;
+import org.graylog.events.notifications.EventNotificationConfig;
 import org.graylog.plugins.teams.alerts.TeamsNotification;
 import org.graylog2.plugin.PluginConfigBean;
 import org.graylog2.plugin.PluginModule;
@@ -9,6 +11,8 @@ import java.util.Set;
 
 public class TeamsModule extends PluginModule {
 
+  private static final String TYPE_NAME = "teams-notification-v2.0.0";
+
   @Override
   public Set<? extends PluginConfigBean> getConfigBeans() {
     return Collections.emptySet();
@@ -17,5 +21,6 @@ public class TeamsModule extends PluginModule {
   @Override
   protected void configure() {
     addAlarmCallback(TeamsNotification.class);
+    addNotificationType(TYPE_NAME, EventNotificationConfig.class, EventNotification.class, EventNotification.Factory.class);
   }
 }
